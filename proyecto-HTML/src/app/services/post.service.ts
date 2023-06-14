ace7556de919f1b216c7b76c6773c5b1f2e9f7eb
pinaalu <pinaalu@uade.edu.ar>
2023-06-13 21:40:40 -0300

    21:40 martes

--- proyecto-HTML/src/app/services/post.service.ts
+++ proyecto-HTML/src/app/services/post.service.ts
@@ -19,10 +19,6 @@ export class PostService {
     return this.http.get(this.url + '/getAll');
   }
 
-  getById(id: number): Observable<any> {
-    return this.http.get(this.url + '/' + id);
-  }
-
   edit(post: Post): Observable<any> {
     return this.http.post(this.url + '/' + post.id + '/update', post);
   }
